23ca252c4d5c5e0b8e514c7db4e259ce8a42aea7
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-11-05 23:31:26 +0200

    Components and css is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { GlobalStyle } from "GlobalStyle";
+import { AppWrapper } from './App.styled';
+
+
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+
+  handleButtonClick = (type) => {
+    this.setState((prevState) => ({
+      ...prevState,
+      [type]: prevState[type] + 1
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+    render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+    // let feedbackContent;
+
+// if (totalFeedback === 0) {
+//   feedbackContent = <Notification message="There is no feedback" />;
+// } else {
+//   feedbackContent = (
+//     <Statistics
+//       good={good}
+//       neutral={neutral}
+//       bad={bad}
+//       total={totalFeedback}
+//       positivePercentage={positiveFeedbackPercentage}
+//     />
+//   );
+// }
+
+return (
+  <AppWrapper>
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={this.handleButtonClick}
+      />
+    </Section>
+
+    <Section title="Statistics">
+      <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      total={totalFeedback}
+      positivePercentage={positiveFeedbackPercentage}
+    />
+    </Section>
+  </AppWrapper>
+);
+
+  }
+}
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+max-width:100%;
+width: 400px;
+`;
+
+export const Item = styled.p`
+text-align: center;
+color: ${p => p.theme.colors.white};
+`;
+
+export const Value = styled.span`
+margin-left: ${p => p.theme.spacing(1)};
+color: ${p => p.theme.colors.white};
+`;
